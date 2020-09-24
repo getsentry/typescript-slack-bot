e754d6312608a2e1de05614e1a98e7628bc77953
Billy Vong <billy@sentry.io>
2020-09-24 15:22:18 -0700

    build: change gcloud projects

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 args="
-  --account=${GOOGLE_SERVICE_ACCOUNT_EMAIL}
   --allow-unauthenticated \
-  --project=sercy-2de63 \
+  --project=sentry-dev-tooling \
   --runtime=nodejs10 \
   --trigger-http \
   --memory=128MB \
